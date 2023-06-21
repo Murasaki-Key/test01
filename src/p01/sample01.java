8e75aec02b7ac7520bb5359ae3ea13afd33019e5
id <id@AQ-0037>
2023-06-21 15:35:24 +0900

    プログラムを追加

--- src/p01/sample01.java
+++ src/p01/sample01.java
@@ -1,7 +1,9 @@
 package p01;
 
 public class sample01 {
- public static void main(String[] args) {
-	 System.out.print("村崎貴心");
- } 
+	public static void main(String[] args) {
+		System.out.println("村崎貴心");
+		System.out.println("23");
+		
+	}
 }
